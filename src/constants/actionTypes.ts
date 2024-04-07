d8b9ef6ed492718a9eeda0cc725218d7cb6537f5
Ibrahim Gaber <ibrahimseda322@gmail.com>
2024-04-07 07:24:37 +0200

    Logout added

--- src/constants/actionTypes.ts
+++ src/constants/actionTypes.ts
@@ -2,3 +2,4 @@ export const LOGIN = "auth/login";
 export const REGISTER = "auth/register";
 export const REFRESHTOKEN = "auth/refreshToken";
 export const USERDATA = "auth/userData";
+export const LOGOUT = "auth/logout";
