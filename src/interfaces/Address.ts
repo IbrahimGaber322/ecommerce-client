ae7091012b93c661c71a238bef4f7d4276c6373c
Yousef Elsayed <yousef4511@outlook.com>
2024-04-13 02:09:07 +0200

    Finish Checkout page

--- src/interfaces/Address.ts
+++ src/interfaces/Address.ts
@@ -1,9 +1,7 @@
 export default interface Address {
   id: number;
-  address_name: string;
   address: string;
-  landline_number: string;
   mobile_number: string;
-  additional_desc: string;
+  desc: string;
   user: number;
 }
--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -4,24 +4,76 @@ import Address from "../interfaces/Address";
 import { useEffect } from "react";
 import { getAddresses } from "../store/address/addressActions";
 import { useAppDispatch } from "../hooks/redux";
-import { Container } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  Typography,
+} from "@mui/material";
+import { useForm, SubmitHandler } from "react-hook-form";
 import ShippingAddressForm from "../components/ShippingAddressForm";
 
+interface ShippingAddressForm {
+  savedAddress: string;
+}
 export default function Checkout() {
   const dispatch = useAppDispatch();
   const addresses: Address[] = useSelector(selectAddresses);
   useEffect(() => {
     dispatch(getAddresses());
+  }, [dispatch]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      savedAddress: "",
+    },
   });
+
+  const onSubmit: SubmitHandler<ShippingAddressForm> = (data) => {
+    console.log(data);
+  };
 
   return (
     <>
       <Container maxWidth="lg" sx={{ mt: "1rem" }}>
-        <h1>Checkout Works</h1>
-        {addresses.map((address) => (
-          <h3 key={address.id}>{address.address}</h3>
-        ))}
-        <ShippingAddressForm></ShippingAddressForm>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Box>
+            <FormControl>
+              <FormLabel>Choose Shipping Address</FormLabel>
+              <RadioGroup name="shipping-address">
+                {addresses.map((address) => (
+                  <FormControlLabel
+                    value={address.id}
+                    control={<Radio />}
+                    label={address.address}
+                    key={address.id}
+                    {...register("savedAddress", {
+                      required: "Must choose a shipping address",
+                    })}
+                  />
+                ))}
+              </RadioGroup>
+              {errors.savedAddress && (
+                <Typography variant="body2" color="error">
+                  {errors.savedAddress.message}
+                </Typography> // Display error message
+              )}
+            </FormControl>
+          </Box>
+          <Button type="submit" variant="contained">
+            Pay
+          </Button>
+        </form>
+        <ShippingAddressForm />
       </Container>
     </>
   );
