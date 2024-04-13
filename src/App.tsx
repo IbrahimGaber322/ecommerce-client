53bd72e22c85faa1b3b468d563c268510dd21123
AlaaSherif55 <alaa.sherif.fathi@gmail.com>
2024-04-13 02:28:29 +0200

    add address component

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/Footer";
 import NotFound from "./pages/NotFound";
+import Address from "./components/Address";
 /**
  * Main application component that handles routing and theme switching.
  */
@@ -114,6 +115,7 @@ function App() {
             />
             <Route path="/verify-email/:token" element={<VerifyEmail />} />
             <Route path="/verify-email" element={<VerifyEmail />} />
+            <Route path="/address" element={<Address />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
           <Box marginTop={"auto"}>
