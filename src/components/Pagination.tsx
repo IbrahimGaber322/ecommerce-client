cefca617d3a408e685d372c4fe99f930ba6e4fc6
Ibrahim Gaber <ibrahimseda322@gmail.com>
2024-04-15 06:31:21 +0200

    Pagination update

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,5 +1,4 @@
-import "../styles/Pagination.css";
-import { Box, Button, IconButton, Paper } from "@mui/material";
+import { Box, Button, IconButton, Paper, Typography } from "@mui/material";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import FirstPageIcon from "@mui/icons-material/FirstPage";
@@ -19,48 +18,39 @@ export default function Pagination({
   eventsNumber: number;
 }) {
   return (
-    <Paper
-      sx={{ justifyContent: "center" }}
-      className="flex items-center justify-between border-t border-gray-200 px-4 py-3 sm:px-6"
-    >
-      <Box
-        sx={{ gap: 2 }}
-        className="flex flex-1 items-center justify-between flex-col sm:flex-row"
-      >
-        <div className="py-2">
-          <p className="text-sm">
+    <Paper sx={{ justifyContent: "center", flexWrap: "wrap", py: 1 }}>
+      <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <Typography sx={{ fontSize: "0.9rem" }}>
             Showing{" "}
-            <span className="font-medium">
+            <span style={{ fontSize: "1rem" }}>
               {page * eventsPerPage - eventsPerPage + 1}
             </span>{" "}
             to{" "}
-            <span className="font-medium">
+            <span style={{ fontSize: "1rem" }}>
               {page * eventsPerPage >= eventsNumber
                 ? eventsNumber
                 : page * eventsPerPage}
             </span>{" "}
-            of <span className="font-medium">{eventsNumber}</span> results
-          </p>
-        </div>
+            of <span style={{ fontSize: "1rem" }}>{eventsNumber}</span> results
+          </Typography>
+        </Box>
 
         <Box
-          sx={{ gap: 1 }}
-          className="isolate inline-flex -space-x-px rounded-md shadow-sm"
-          aria-label="Pagination"
+          sx={{
+            gap: 1,
+            display: "flex",
+            justifyContent: "center",
+            flexWrap: "wrap",
+          }}
         >
           {page - 3 > 1 && (
-            <IconButton
-              onClick={() => setPage(1)}
-              className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-            >
+            <IconButton onClick={() => setPage(1)}>
               <FirstPageIcon />
             </IconButton>
           )}
 
-          <IconButton
-            onClick={() => page !== 1 && setPage(page - 1)}
-            className="relative inline-flex items-center  px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-          >
+          <IconButton onClick={() => page !== 1 && setPage(page - 1)}>
             <ChevronLeftIcon />
           </IconButton>
 
@@ -72,27 +62,29 @@ export default function Pagination({
                 <Button
                   key={i}
                   onClick={() => setPage(i + 1)}
-                  className={`${
+                  sx={
                     page === i + 1
-                      ? "bg-orange-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
-                      : "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-300 focus:outline-offset-0"
-                  } relative inline-flex items-center px-4 py-2 text-sm font-semibold focus:z-20`}
+                      ? {
+                          color: "primary.main",
+                          "&:hover": { bgcolor: "inherit", cursor: "default" },
+                        }
+                      : {
+                          color: "secondary.main",
+                          "&:hover": {
+                            color: "primary.main",
+                          },
+                        }
+                  }
                 >
                   {i + 1}
                 </Button>
               )
           )}
-          <IconButton
-            onClick={() => page !== pages && setPage(page + 1)}
-            className="relative inline-flex items-center  px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-          >
+          <IconButton onClick={() => page !== pages && setPage(page + 1)}>
             <ChevronRightIcon />
           </IconButton>
           {page + 3 < pages && (
-            <IconButton
-              onClick={() => setPage(pages)}
-              className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-            >
+            <IconButton onClick={() => setPage(pages)}>
               <LastPageIcon />
             </IconButton>
           )}
